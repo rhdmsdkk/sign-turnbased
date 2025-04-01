ae607abecff7e50375cc9c8f863c04339d660ce4
Rachel Kim <rachelgekim@gmail.com>
2025-04-01 00:20:14 -0400

    add movement on inputs

--- Assets/Rachel/Scripts/PlayerMovement.cs
+++ Assets/Rachel/Scripts/PlayerMovement.cs
@@ -1,11 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.OnScreen;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Attributes")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool isJumping;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
-        
+        if (Gamepad.current != null && Gamepad.current.leftStick.ReadValue() != Vector2.zero)
+        {
+            moveInput = Gamepad.current.leftStick.ReadValue();
+        }
+        else
+        {
+            moveInput = Vector2.zero;
+        }
+
+        isJumping = (Gamepad.current != null && Gamepad.current.buttonSouth.isPressed);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        if (isJumping && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
     }
 }
