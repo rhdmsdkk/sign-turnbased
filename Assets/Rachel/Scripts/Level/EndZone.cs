0d60618c429239f85f43b96f63929bc5fedaa671
Rachel Kim <rachelgekim@gmail.com>
2025-04-15 13:30:38 -0400

    add level transition logic for combat

--- Assets/Rachel/Scripts/Level/EndZone.cs
+++ Assets/Rachel/Scripts/Level/EndZone.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndZone : MonoBehaviour
 {
+    public bool isCombat = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<PlayerMovement>(out PlayerMovement _))
         {
-            LevelManager.instance.LoadChapter(1);
+            if (isCombat)
+            {
+                string scene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene("Combat_" + scene);
+            }
+            else
+            {
+                LevelManager.instance.LoadChapter(1);
+            }
         }
     }
 }
